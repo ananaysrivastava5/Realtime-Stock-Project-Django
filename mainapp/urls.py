59bb7b5f3e92f3510562a8151c03a47f287e218a
Ananay Srivastava <ananaysrivastava5@gmail.com>
2021-10-03 03:09:38 +0530

    frontend part completed

---
+++ mainapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views 
+
+
+urlpatterns = [
+    path('', views.stockPicker, name='stockpicker'),
+    path('stocktracker/', views.stockTracker, name='stocktracker'),
+]
